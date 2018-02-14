5dc542cd9dec47c354ec394e0adbaa49ca3d6b3e
Varit Assavavisidchai <varit.as@ku.th>
2018-02-14 11:36:08 +0700

    fix issue

--- src/coinpurse/TestPurseMain.java
+++ src/coinpurse/TestPurseMain.java
@@ -13,16 +13,22 @@ public class TestPurseMain {
 	 */
 	public static void main(String[] args) {
 		Purse p = new Purse(10);
-		p.insert(new BankNote(5, "A"));
-		p.insert(new BankNote(10, "A"));
+		p.insert(new Coin(5, "A"));
+		p.insert(new Coin(10, "A"));
 		p.insert(new Coin(1, "A"));
-		p.insert(new BankNote(5, "A"));
+		p.insert(new Coin(5, "A"));
 		p.insert(new Coin(5, "B"));
-		p.insert(new BankNote(5, "E"));
-		p.insert(new BankNote(5, "B"));
+		p.insert(new Coin(5, "E"));
+		p.insert(new Coin(5, "B"));
 		p.insert(new Coin(5, "E"));
 		p.withdraw(10);
 		System.out.println(p.getBalance());
+		BankNote bn = new BankNote(10 , "a");
+		Purse t = new Purse(5);
+		t.insert(bn);
+		System.out.print("getBalance: ");
+		System.out.println(t.getBalance());
+		System.out.println(bn.getValue());
 	}
 
 }
